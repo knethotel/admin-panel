9479420001967ff94a49f1e5075aa08e788103ee
jeetsingh008 <158179537+jeetsingh008@users.noreply.github.com>
2025-03-03 16:29:49 +0530

    Fixed the dynamic routing issue and added payment management page

--- app/redux/slices/userslice.ts
+++ app/redux/slices/userslice.ts
@@ -2,8 +2,6 @@ import { createSlice, PayloadAction } from '@reduxjs/toolkit';
 import { AxiosResponse } from 'axios';
 import { getAllUser } from '../action/useraction';
 
-
-
 interface UserState {
   loading: boolean;
 
@@ -13,7 +11,7 @@ interface UserState {
 
   error: string | null;
   currentPage: number;
-  
+
   totalPages: number;
   IsActive: boolean;
 }
@@ -21,15 +19,15 @@ interface UserState {
 const initialState: UserState = {
   loading: false,
 
-  User:[],
+  User: [],
   selectedUser: null,
   totalUser: 0,
-  
+
   error: null,
   currentPage: 1,
-  
+
   totalPages: 0,
-  IsActive: false,
+  IsActive: false
 };
 
 const userSlice = createSlice({
@@ -38,34 +36,33 @@ const userSlice = createSlice({
   reducers: {
     setCurrentPage(state, action: PayloadAction<any>) {
       state.currentPage = action.payload; // Update currentPage in state
-    },
+    }
   },
   extraReducers: (builder) => {
     builder
 
-    //get all ApproverUser slice
-    .addCase(getAllUser.pending, (state) => {
-      state.loading = true;
-    })
-    .addCase(getAllUser.fulfilled, (state, action: PayloadAction<any>) => {
-      state.loading = false;
-      // console.log(action.payload);
-      state.User = action.payload.data.approver;
-      // console.log(state.cases);
-      state.totalUser = action.payload.total;
-      // console.log(state.totalCases);
-      state.currentPage = action.payload.currentPage;
-      // console.log(state.currentPage);
-      state.totalPages = action.payload.totalPages;
-      // console.log(state.totalPages);
-    })
-    .addCase(getAllUser.rejected, (state, action: PayloadAction<any>) => {
-      state.loading = false;
-      state.error = action.payload as string;
-    })
-
-    },
+      //get all ApproverUser slice
+      .addCase(getAllUser.pending, (state) => {
+        state.loading = true;
+      })
+      .addCase(getAllUser.fulfilled, (state, action: PayloadAction<any>) => {
+        state.loading = false;
+        // console.log(action.payload);
+        state.User = action.payload.data.approver;
+        // console.log(state.cases);
+        state.totalUser = action.payload.total;
+        // console.log(state.totalCases);
+        state.currentPage = action.payload.currentPage;
+        // console.log(state.currentPage);
+        state.totalPages = action.payload.totalPages;
+        // console.log(state.totalPages);
+      })
+      .addCase(getAllUser.rejected, (state, action: PayloadAction<any>) => {
+        state.loading = false;
+        state.error = action.payload as string;
+      });
+  }
 });
 
-export const { setCurrentPage} = userSlice.actions;
+export const { setCurrentPage } = userSlice.actions;
 export default userSlice.reducer;
