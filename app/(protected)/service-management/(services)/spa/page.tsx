eee4c1c2ec3446644d63d44bd039bc53a381cefe
jeetsingh008 <158179537+jeetsingh008@users.noreply.github.com>
2025-03-06 18:52:49 +0530

    Added the spa service page

---
+++ app/(protected)/service-management/(services)/spa/page.tsx
@@ -0,0 +1,12 @@
+import { SpaServiceDataTable } from '@/components/tables/spa-service/client';
+import React from 'react';
+
+const HousekeepingPage = () => {
+  return (
+    <div className="w-full">
+      <SpaServiceDataTable />
+    </div>
+  );
+};
+
+export default HousekeepingPage;
