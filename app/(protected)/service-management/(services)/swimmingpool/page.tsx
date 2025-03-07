8995a487d9451ab6bd31ce28393562409ef05758
jeetsingh008 <158179537+jeetsingh008@users.noreply.github.com>
2025-03-07 12:56:04 +0530

    Added the swimming pool service page

---
+++ app/(protected)/service-management/(services)/swimmingpool/page.tsx
@@ -0,0 +1,12 @@
+import { SwimmingpoolServiceDataTable } from '@/components/tables/swimming-pool-service/client';
+import React from 'react';
+
+const HousekeepingPage = () => {
+  return (
+    <div className="w-full">
+      <SwimmingpoolServiceDataTable />
+    </div>
+  );
+};
+
+export default HousekeepingPage;
