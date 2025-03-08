9c7bc2ba7445627271375fa18fc48c309708ec3a
jeetsingh008 <158179537+jeetsingh008@users.noreply.github.com>
2025-03-08 10:37:58 +0530

    Added the InRoomControl service page

---
+++ app/(protected)/service-management/(services)/in_room_control/page.tsx
@@ -0,0 +1,13 @@
+import { ConciergeServiceTable } from '@/components/tables/concierge/client';
+import { InRoomControlDataTable } from '@/components/tables/In_Room_Control-Service/client';
+import React from 'react';
+
+const HousekeepingPage = () => {
+  return (
+    <div className="w-full">
+      <InRoomControlDataTable />
+    </div>
+  );
+};
+
+export default HousekeepingPage;
