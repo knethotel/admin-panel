61b822d4b9e11c7e9018e29850760eda965f3dcf
jeetsingh008 <158179537+jeetsingh008@users.noreply.github.com>
2025-03-08 14:22:47 +0530

    Updated the text color in order status column values

---
+++ app/(protected)/service-management/(services)/inroomdining/page.tsx
@@ -0,0 +1,12 @@
+import { InRoomDiningDataTable } from '@/components/tables/In_Room_Dining-Service/client';
+import React from 'react';
+
+const HousekeepingPage = () => {
+  return (
+    <div className="w-full">
+      <InRoomDiningDataTable />
+    </div>
+  );
+};
+
+export default HousekeepingPage;
