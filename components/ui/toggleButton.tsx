c8050e6e6da4ee569d72e52afdb30fa2d68c100e
jeetsingh008 <158179537+jeetsingh008@users.noreply.github.com>
2025-03-20 10:46:23 +0530

    Fixed the global component toggle button

--- components/ui/toggleButton.tsx
+++ components/ui/toggleButton.tsx
@@ -8,13 +8,13 @@ export default function ToggleButton() {
   return (
     <button
       onClick={() => setEnabled(!enabled)}
-      className={`relative w-12 h-5 py-2 flex items-center bg-gray-300 rounded-full p-1 transition duration-300 ${
-        enabled ? 'bg-yellow-950' : 'bg-gray-300'
+      className={`relative h-[19.5px] w-[46px] py-2 flex items-center rounded-full p-1 transition duration-300 ${
+        enabled ? 'bg-coffee' : 'bg-brown'
       }`}
     >
       <div
-        className={`w-4 h-4 bg-white rounded-full shadow-md transform transition duration-300 ${
-          enabled ? 'translate-x-6' : 'translate-x-0'
+        className={`h-[15px] w-[15px] bg-white rounded-full shadow-md transform transition duration-300 ${
+          enabled ? 'translate-x-[24.5px]' : '-translate-x-[0.5px]'
         }`}
       />
       <span className="absolute left-2 text-xs"></span>
