bc86675cd58b5d86e9d0cb7127d1262c643f15d4
jeetsingh008 <158179537+jeetsingh008@users.noreply.github.com>
2025-03-20 11:04:55 +0530

    Updated the toggle button style

--- components/ui/toggleButton.tsx
+++ components/ui/toggleButton.tsx
@@ -8,13 +8,13 @@ export default function ToggleButton() {
   return (
     <button
       onClick={() => setEnabled(!enabled)}
-      className={`relative h-[19.5px] shadow-sm w-[46px] py-2 flex items-center rounded-full p-1 transition duration-300 ${
+      className={`relative h-[19.5px] shadow-sm w-[44px] py-2 flex items-center rounded-full p-1 transition duration-300 ${
         enabled ? 'bg-coffee' : 'bg-brown'
       }`}
     >
       <div
         className={`h-[17px] w-[17px] rounded-full shadow-md transform transition duration-300 ${
-          enabled ? 'translate-x-[25px] bg-brown' : '-translate-x-[3px] bg-coffee'
+          enabled ? 'translate-x-[23px] bg-brown' : '-translate-x-[4px] bg-coffee'
         }`}
       />
       <span className="absolute left-2 text-xs"></span>
