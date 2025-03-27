9160dc5e9f1673d60e67dc257f900816e8535497
jeetsingh008 <158179537+jeetsingh008@users.noreply.github.com>
2025-03-27 11:27:34 +0530

    Added the accidently deleted hotel profile page again

---
+++ app/(protected)/hotel-profile/page.tsx
@@ -0,0 +1,12 @@
+import HotelForm from '@/components/form/hotel-profile/hotel-form';
+import React from 'react';
+
+const HotelProfile = () => {
+  return (
+    <div className="flex flex-col min-h-screen w-full bg-[#FAF8F5] py-4">
+      <HotelForm />
+    </div>
+  );
+};
+
+export default HotelProfile;
