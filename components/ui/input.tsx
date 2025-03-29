0578798f7bc16daee2e880cd545e04ac74e321c6
jeetsingh008 <158179537+jeetsingh008@users.noreply.github.com>
2025-03-29 18:01:28 +0530

    Changed styling of card and input box

--- components/ui/input.tsx
+++ components/ui/input.tsx
@@ -18,7 +18,7 @@ const Input = React.forwardRef<HTMLInputElement, InputProps>(
         <input
           type={type}
           className={cn(
-            'flex h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none disabled:cursor-not-allowed',
+            'flex h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-sm focus:ring-1 focus:ring-coffee ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none disabled:cursor-not-allowed',
             icon ? 'pl-1' : '', // Add left padding if icon exists
             className
           )}
