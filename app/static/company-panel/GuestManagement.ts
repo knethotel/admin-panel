bc94a38d8d8accd23bb4487a97e0dc17c7aac1a4
jeetsingh008 <158179537+jeetsingh008@users.noreply.github.com>
2025-03-30 22:49:48 +0530

    Added dummy data for table

---
+++ app/static/company-panel/GuestManagement.ts
@@ -0,0 +1,228 @@
+export type GuestDetailsDataType = {
+  guestID: string;
+  hotelName: string;
+  guestName: string;
+  mobileNo: string;
+  roomCategory: string;
+  checkInDetails: {
+    date: string;
+    time: string;
+  };
+  checkOutDetails: {
+    date: string;
+    time: string;
+  };
+};
+
+export const GuestDetailsDummyData: GuestDetailsDataType[] = [
+  {
+    guestID: 'GD123452',
+    hotelName: 'Hilton',
+    guestName: 'John Doe',
+    mobileNo: '9835081173',
+    roomCategory: 'SINGLE ROOM',
+    checkInDetails: {
+      date: '10-02-25',
+      time: '10:00'
+    },
+    checkOutDetails: {
+      date: '12-02-25',
+      time: '11:00'
+    }
+  },
+  {
+    guestID: 'GD123452',
+    hotelName: 'Hilton',
+    guestName: 'John Doe',
+    mobileNo: '9835081173',
+    roomCategory: 'SINGLE ROOM',
+    checkInDetails: {
+      date: '10-02-25',
+      time: '10:00'
+    },
+    checkOutDetails: {
+      date: '12-02-25',
+      time: '11:00'
+    }
+  },
+  {
+    guestID: 'GD123452',
+    hotelName: 'Hilton',
+    guestName: 'John Doe',
+    mobileNo: '9835081173',
+    roomCategory: 'SINGLE ROOM',
+    checkInDetails: {
+      date: '10-02-25',
+      time: '10:00'
+    },
+    checkOutDetails: {
+      date: '12-02-25',
+      time: '11:00'
+    }
+  },
+  {
+    guestID: 'GD123452',
+    hotelName: 'Hilton',
+    guestName: 'John Doe',
+    mobileNo: '9835081173',
+    roomCategory: 'SINGLE ROOM',
+    checkInDetails: {
+      date: '10-02-25',
+      time: '10:00'
+    },
+    checkOutDetails: {
+      date: '12-02-25',
+      time: '11:00'
+    }
+  },
+  {
+    guestID: 'GD123452',
+    hotelName: 'Hilton',
+    guestName: 'John Doe',
+    mobileNo: '9835081173',
+    roomCategory: 'SINGLE ROOM',
+    checkInDetails: {
+      date: '10-02-25',
+      time: '10:00'
+    },
+    checkOutDetails: {
+      date: '12-02-25',
+      time: '11:00'
+    }
+  },
+  {
+    guestID: 'GD123452',
+    hotelName: 'Hilton',
+    guestName: 'John Doe',
+    mobileNo: '9835081173',
+    roomCategory: 'SINGLE ROOM',
+    checkInDetails: {
+      date: '10-02-25',
+      time: '10:00'
+    },
+    checkOutDetails: {
+      date: '12-02-25',
+      time: '11:00'
+    }
+  },
+  {
+    guestID: 'GD123452',
+    hotelName: 'Hilton',
+    guestName: 'John Doe',
+    mobileNo: '9835081173',
+    roomCategory: 'SINGLE ROOM',
+    checkInDetails: {
+      date: '10-02-25',
+      time: '10:00'
+    },
+    checkOutDetails: {
+      date: '12-02-25',
+      time: '11:00'
+    }
+  },
+  {
+    guestID: 'GD123452',
+    hotelName: 'Hilton',
+    guestName: 'John Doe',
+    mobileNo: '9835081173',
+    roomCategory: 'SINGLE ROOM',
+    checkInDetails: {
+      date: '10-02-25',
+      time: '10:00'
+    },
+    checkOutDetails: {
+      date: '12-02-25',
+      time: '11:00'
+    }
+  },
+  {
+    guestID: 'GD123452',
+    hotelName: 'Hilton',
+    guestName: 'John Doe',
+    mobileNo: '9835081173',
+    roomCategory: 'SINGLE ROOM',
+    checkInDetails: {
+      date: '10-02-25',
+      time: '10:00'
+    },
+    checkOutDetails: {
+      date: '12-02-25',
+      time: '11:00'
+    }
+  },
+  {
+    guestID: 'GD123452',
+    hotelName: 'Hilton',
+    guestName: 'John Doe',
+    mobileNo: '9835081173',
+    roomCategory: 'SINGLE ROOM',
+    checkInDetails: {
+      date: '10-02-25',
+      time: '10:00'
+    },
+    checkOutDetails: {
+      date: '12-02-25',
+      time: '11:00'
+    }
+  },
+  {
+    guestID: 'GD123452',
+    hotelName: 'Hilton',
+    guestName: 'John Doe',
+    mobileNo: '9835081173',
+    roomCategory: 'SINGLE ROOM',
+    checkInDetails: {
+      date: '10-02-25',
+      time: '10:00'
+    },
+    checkOutDetails: {
+      date: '12-02-25',
+      time: '11:00'
+    }
+  },
+  {
+    guestID: 'GD123452',
+    hotelName: 'Hilton',
+    guestName: 'John Doe',
+    mobileNo: '9835081173',
+    roomCategory: 'SINGLE ROOM',
+    checkInDetails: {
+      date: '10-02-25',
+      time: '10:00'
+    },
+    checkOutDetails: {
+      date: '12-02-25',
+      time: '11:00'
+    }
+  },
+  {
+    guestID: 'GD123452',
+    hotelName: 'Hilton',
+    guestName: 'John Doe',
+    mobileNo: '9835081173',
+    roomCategory: 'SINGLE ROOM',
+    checkInDetails: {
+      date: '10-02-25',
+      time: '10:00'
+    },
+    checkOutDetails: {
+      date: '12-02-25',
+      time: '11:00'
+    }
+  },
+  {
+    guestID: 'GD123452',
+    hotelName: 'Hilton',
+    guestName: 'John Doe',
+    mobileNo: '9835081173',
+    roomCategory: 'SINGLE ROOM',
+    checkInDetails: {
+      date: '10-02-25',
+      time: '10:00'
+    },
+    checkOutDetails: {
+      date: '12-02-25',
+      time: '11:00'
+    }
+  }
+];
