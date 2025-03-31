ec0ec59c3fd65435024236fb403e9098f36bfe24
jeetsingh008 <158179537+jeetsingh008@users.noreply.github.com>
2025-03-31 12:16:17 +0530

    Added the schema

--- schema/company-panel/index.ts
+++ schema/company-panel/index.ts
@@ -1,7 +1,7 @@
 import { z } from 'zod';
 // ****************************Company Panel*********************
 
-// Admin Management Schema
+// Admin Management Schema---------------------------------------------------------------------------------------
 export const adminSchema = z.object({
   firstName: z.string().min(1, 'First Name is required'),
   lastName: z.string().min(1, 'Last Name is required'),
@@ -20,3 +20,28 @@ export const adminSchema = z.object({
 });
 
 export type adminSchemaType = z.infer<typeof adminSchema>;
+
+// Subscription Management Schema---------------------------------------------------------------------------------
+export const SubscriptionManagementFormSchema = z.object({
+  subscriptionID: z
+    .string()
+    .min(1, 'Subscription ID cannot be empty')
+    .optional(),
+  planName: z.string().min(1, 'Plan Name cannot be empty').optional(),
+  planDuration: z.string().min(1, 'Plan Duration cannot be empty').optional(), // Made optional
+  planType: z.string().min(1, 'Plan Type cannot be empty').optional(),
+  description: z.string().min(1, 'Description cannot be empty').optional(),
+  status: z.enum(['Active', 'Inactive', 'Cancelled', 'Expired'], {
+    errorMap: () => ({ message: 'Invalid status' })
+  }),
+  cost: z.coerce
+    .number({ invalid_type_error: 'Cost must be a valid number' })
+    .positive('Cost must be positive')
+    .min(0, 'Cost must be non-negative')
+    .optional() // Made optional
+  // PAYMENT TYPE FIELD VALIDATION IS LEFT
+});
+
+export type SubscriptionManagementFormSchemaType = z.infer<
+  typeof SubscriptionManagementFormSchema
+>;
