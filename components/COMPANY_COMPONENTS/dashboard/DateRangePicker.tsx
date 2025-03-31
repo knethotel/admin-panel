736ce6e5ba203ca515501d6b947ec5c71a988f81
jeetsingh008 <158179537+jeetsingh008@users.noreply.github.com>
2025-03-31 12:57:52 +0530

    Added the dashboard page, made all the components of it

---
+++ components/COMPANY_COMPONENTS/dashboard/DateRangePicker.tsx
@@ -0,0 +1,25 @@
+'use client';
+import React from 'react';
+import { useState } from 'react';
+import DatePicker from 'react-datepicker';
+import 'react-datepicker/dist/react-datepicker.css';
+
+const DateRangePickerComponent = () => {
+  const [dateRange, setDateRange] = useState([null, null]);
+  const [startDate, endDate] = dateRange;
+  return (
+    <DatePicker
+      className="bg-transparent text-white text-xs focus:outline-none border border-amber-600 border-opacity-60 placeholder:text-xs px-1 py-1 rounded-md"
+      selectsRange={true}
+      startDate={startDate}
+      endDate={endDate}
+      onChange={(update: any) => {
+        setDateRange(update);
+      }}
+      isClearable={true}
+      placeholderText="Select Date Range"
+    />
+  );
+};
+
+export default DateRangePickerComponent;
---
+++ components/COMPANY_COMPONENTS/dashboard/StackedBarChart.tsx
@@ -0,0 +1,112 @@
+'use client';
+
+import { Bar, BarChart, XAxis } from 'recharts';
+import { Card, CardContent } from '@/components/ui/card';
+import {
+  ChartConfig,
+  ChartContainer,
+  ChartLegend,
+  ChartLegendContent,
+  ChartTooltip,
+  ChartTooltipContent
+} from '@/components/ui/chart';
+
+type chartDataType = {
+  service: string;
+  totalRevenue: number;
+  profit: number;
+  losses: number;
+};
+
+const chartConfig = {
+  totalRevenue: {
+    label: 'Total Revenue',
+    color: 'hsl(var(--chart-4))'
+  },
+  profit: {
+    label: 'Profit',
+    color: 'hsl(var(--chart-5))'
+  },
+  losses: {
+    label: 'Losses',
+    color: 'hsl(var(--chart-6))'
+  }
+} satisfies ChartConfig;
+
+const CustomTick = (props: any) => {
+  const { x, y, payload } = props;
+  const words = payload.value.split(' ');
+
+  return (
+    <g transform={`translate(${x},${y})`}>
+      <foreignObject width={100} height={60} x={-50} y={0}>
+        <div
+          style={{
+            display: 'flex',
+            flexDirection: 'column',
+            textAlign: 'center',
+            fontSize: '10px',
+            lineHeight: '1.2'
+          }}
+        >
+          {words.map((word: string, i: number) => (
+            <span key={i}>{word}</span>
+          ))}
+        </div>
+      </foreignObject>
+    </g>
+  );
+};
+
+export function StackedBarChart({ chartData }: { chartData: chartDataType[] }) {
+  return (
+    <Card className="w-full bg-transparent">
+      <CardContent>
+        <ChartContainer
+          className="bg-transparent h-72 w-full"
+          config={chartConfig}
+        >
+          <BarChart
+            accessibilityLayer
+            data={chartData}
+            // margin={{ bottom: 60 }} // Increased bottom margin for stacked labels
+          >
+            <XAxis
+              className="text-white/60"
+              dataKey="service"
+              tickLine={false}
+              tickMargin={10}
+              axisLine={false}
+              tick={<CustomTick />}
+              interval={0}
+              height={40} // Increased height to accommodate stacked labels
+            />
+            <ChartTooltip content={<ChartTooltipContent hideLabel />} />
+            <ChartLegend content={<ChartLegendContent />} />
+            <Bar
+              dataKey="totalRevenue"
+              stackId="a"
+              fill="var(--color-totalRevenue)"
+              radius={[5, 5, 5, 5]}
+              barSize={45}
+            />
+            <Bar
+              dataKey="profit"
+              stackId="a"
+              fill="var(--color-profit)"
+              radius={[5, 5, 5, 5]}
+              barSize={45}
+            />
+            <Bar
+              dataKey="losses"
+              stackId="a"
+              fill="var(--color-losses)"
+              radius={[5, 5, 5, 5]}
+              barSize={45}
+            />
+          </BarChart>
+        </ChartContainer>
+      </CardContent>
+    </Card>
+  );
+}
