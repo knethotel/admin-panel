7f63866aa06d7090dd7871c81d749e7c691ee154
jeetsingh008 <158179537+jeetsingh008@users.noreply.github.com>
2025-04-01 15:33:54 +0530

    Added the sub-hotel management page

---
+++ app/(protected)/(company-panel)/company/sub_hotel-management/page.tsx
@@ -0,0 +1,12 @@
+import { SubHotelManagementHome } from '@/components/COMPANY_COMPONENTS/sub_hotel-management/table/client';
+import React from 'react';
+
+const SubHotelManagementPage = () => {
+  return (
+    <div className="h-screen w-full">
+      <SubHotelManagementHome />
+    </div>
+  );
+};
+
+export default SubHotelManagementPage;
---
+++ app/(protected)/(company-panel)/company/sub_hotel-management/view/[id]/page.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import SubHotelIdForm from '@/components/COMPANY_COMPONENTS/sub_hotel-management/form/sub_hotel-form';
+const ViewSubHotelDetailsPage = async ({
+  params
+}: {
+  params: Promise<{ id: string }>;
+}) => {
+  const id = (await params).id;
+  return (
+    <div className="flex justify-center items-center w-full px-14 py-10">
+      <SubHotelIdForm subHotelID={id} mode="view" />
+    </div>
+  );
+};
+
+export default ViewSubHotelDetailsPage;
