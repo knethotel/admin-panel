17183a9fa0ebc33f46c2fe6912f62d7cd802b33c
jeetsingh008 <158179537+jeetsingh008@users.noreply.github.com>
2025-04-01 10:30:54 +0530

    Defined the hotel management form schema

--- schema/company-panel/index.ts
+++ schema/company-panel/index.ts
@@ -66,3 +66,59 @@ export const complaintFormSchema = z.object({
 });
 
 export type ComplaintFormSchemaType = z.infer<typeof complaintFormSchema>;
+
+// Hotel management form schema--------------------------------------------------------------------------------
+
+export const serviceOptions = [
+  'Reception',
+  'Housekeeping',
+  'In-Room Dining',
+  'Gym',
+  'Spa',
+  'Swimming Pool',
+  'Concierge Service',
+  'In-Room Control',
+  'Order Management',
+  'SOS Management',
+  'Chat With Staff'
+] as const;
+
+export type ServiceType = (typeof serviceOptions)[number];
+
+export const CreateHotelIdFormSchema = z.object({
+  hotelImageUrl: z.string(),
+  hotelImageFile: z
+    .custom<File | undefined>(
+      (file) => file instanceof File || typeof file === 'undefined',
+      { message: 'Invalid file format' }
+    )
+    .refine(
+      (file) =>
+        !file ||
+        ['image/jpeg', 'image/png', 'image/gif', 'image/webp'].includes(
+          file.type
+        ),
+      { message: 'Only JPG, PNG, GIF, and WEBP formats are allowed.' }
+    )
+    .refine((file) => !file || file.size <= 5 * 1024 * 1024, {
+      message: 'Image size must be 5MB or less.'
+    }),
+  hotelID: z.string().min(1, 'Hotel ID cannot be empty'),
+  hotelName: z.string().min(1, 'Hotel Name cannot be empty'), // Typo: "enmtpy" -> "empty"
+  address: z.string().min(1, 'Address field is required'),
+  services: z.array(z.enum(serviceOptions)),
+  subscriptionPlan: z.string().min(1, 'Subscription plan name is required.'),
+  subscriptionPrice: z.number().min(1, 'Price is required.'),
+  contactNo: z
+    .string()
+    .length(10, 'Contact number must be exactly 10 digits')
+    .regex(/^\d+$/, 'Contact number must contain only digits'),
+  email: z
+    .string()
+    .email('Invalid email address')
+    .min(1, 'Email address is required')
+});
+
+export type CreateHotelIdFormSchemaType = z.infer<
+  typeof CreateHotelIdFormSchema
+>;
