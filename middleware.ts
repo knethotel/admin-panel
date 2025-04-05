93c95ed849e53b209e7b8237a7418ab6536d6ae0
jeetsingh008 <158179537+jeetsingh008@users.noreply.github.com>
2025-04-05 17:08:02 +0530

    Added the temporary middleware func.

--- middleware.ts
+++ middleware.ts
@@ -1,12 +1,13 @@
 import { NextResponse } from 'next/server';
 
 export function middleware(req: any) {
-  const adminData = req.cookies.get('admin'); // Alternatively, use session storage
+  // const adminData = req.cookies.get('admin'); // Alternatively, use session storage
 
-  // Redirect unauthenticated users trying to access protected routes
-  if (!adminData?.token && req.nextUrl.pathname.startsWith('/super-admin')) {
-    return NextResponse.redirect(new URL('/', req.url));
-  }
+  // // Redirect unauthenticated users trying to access protected routes
+  // if (!adminData?.token && req.nextUrl.pathname.startsWith('/super-admin')) {
+  //   return NextResponse.redirect(new URL('/', req.url));
+  // }
 
   return NextResponse.next();
 }
+
