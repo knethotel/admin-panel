63676b9867129847c0f585eff74da5861334bb78
jeetsingh008 <158179537+jeetsingh008@users.noreply.github.com>
2025-04-07 18:48:52 +0530

    Added the view action button, and the logic

--- components/COMPANY_COMPONENTS/admin-management/tables/cell-action.tsx
+++ components/COMPANY_COMPONENTS/admin-management/tables/cell-action.tsx
@@ -1,6 +1,6 @@
 import { Button } from '@/components/ui/button';
 import ToggleButton from '@/components/ui/toggleButton';
-import { Edit } from 'lucide-react';
+import { Edit, Eye } from 'lucide-react';
 import { useRouter } from 'next/navigation';
 import React from 'react';
 
@@ -14,11 +14,21 @@ const CellAction = (props: any) => {
     // console.log('success');
   };
 
+  const handleViewUser = () => {
+    router.push(`/super-admin/admin-management/view/${data._id}`);
+  };
+
   return (
     <>
       {/* Action Buttons */}
       <div className="flex items-center space-x-2">
         {/* Edit User */}
+        <button
+          onClick={() => handleViewUser()}
+          className="p-1 rounded-md group hover:bg-[#a07d3d5e]"
+        >
+          <Eye className=" w-4 text-button-dark group-hover:text-white" />
+        </button>
         <Button
           onClick={() => handleEditUser()}
           className="p-3 rounded-md group cursor-pointer hover:bg-[#a07d3d5e]"
