2192db95785e5bcb2ca29be0f62e7388a89fa849
jeetsingh008 <158179537+jeetsingh008@users.noreply.github.com>
2025-04-09 08:50:41 +0530

    Removed the userSlice that was prewritten code

--- app/redux/store.ts
+++ app/redux/store.ts
@@ -1,14 +1,9 @@
-'use-client'
+'use-client';
 import { configureStore } from '@reduxjs/toolkit';
-import UserReducer from './slices/userslice';
-
 
 export const store = configureStore({
-  reducer: {
-    user:UserReducer,
-  },
+  reducer: {}
 });
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
-
