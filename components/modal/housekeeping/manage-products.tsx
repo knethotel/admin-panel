4391a6e1216cc89df580e433054b4605768a9e97
jeetsingh008 <158179537+jeetsingh008@users.noreply.github.com>
2025-04-16 17:58:26 +0530

    Made the housekeeping product details page

---
+++ components/modal/housekeeping/manage-products.tsx
@@ -0,0 +1,248 @@
+'use client';
+import { zodResolver } from '@hookform/resolvers/zod';
+import React, { useEffect, useState } from 'react';
+import { useForm } from 'react-hook-form';
+import { useRouter } from 'next/navigation';
+import {
+  ManageProductsModalFormSchema,
+  ManageProductsModalFormSchemaType
+} from 'schema';
+import { PiCameraThin } from 'react-icons/pi';
+import {
+  Form,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormControl,
+  FormMessage
+} from '../../ui/form';
+import { Input } from '../../ui/input';
+import { Button } from '../../ui/button';
+import { X } from 'lucide-react';
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue
+} from '@/components/ui/select';
+
+interface ModalProps {
+  isOpen: boolean;
+  onClose: () => void;
+}
+
+const AddItemModal: React.FC<ModalProps> = ({ isOpen, onClose }) => {
+  const router = useRouter();
+  const [preview, setPreview] = useState<string | null>(null);
+
+  useEffect(() => {
+    return () => {
+      if (preview) URL.revokeObjectURL(preview);
+    };
+  }, [preview]);
+
+  const form = useForm<ManageProductsModalFormSchemaType>({
+    resolver: zodResolver(ManageProductsModalFormSchema),
+    defaultValues: {
+      selectService: 'Laundary Service',
+      productCategory: '',
+      productName: ''
+    }
+  });
+
+  const onSubmit = (data: ManageProductsModalFormSchemaType) => {
+    console.log('Submitted Data:', data);
+    form.reset();
+  };
+
+  if (!isOpen) return null;
+
+  return (
+    <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 backdrop-blur-sm z-50">
+      <div className="bg-[#FAF6EF] rounded-lg shadow-lg pt-4 pb-8 w-full max-w-5xl relative animate-fadeIn">
+        <X
+          onClick={onClose}
+          className="absolute top-6 right-6 text-gray-600 hover:text-black"
+        />
+
+        <Form {...form}>
+          <form
+            onSubmit={form.handleSubmit(onSubmit)}
+            className="flex flex-col gap-6 text-sm"
+          >
+            {/* Line */}
+            <div className="w-full h-[1px] bg-black opacity-20 mt-12" />
+            {/* Lower part of the form */}
+            <div className="flex justify-between items-start px-10">
+              <div className="w-[60%] flex flex-col gap-6">
+                {/* Select Service */}
+                <FormField
+                  control={form.control}
+                  name="selectService"
+                  render={({ field }) => (
+                    <FormItem className="flex items-center gap-4">
+                      <FormLabel className="text-sm w-[129px] text-nowrap font-medium text-gray-700">
+                        Product category
+                      </FormLabel>
+                      <FormControl>
+                        <Select
+                          onValueChange={field.onChange}
+                          value={field.value}
+                        >
+                          <SelectTrigger className="w-48 bg-lightbrown text-gray-700 p-2 rounded-md border-none">
+                            <SelectValue placeholder="Select type" />
+                          </SelectTrigger>
+                          <SelectContent className="bg-[#362913] rounded-2xl text-white border-2 shadow-md border-white">
+                            {['Laundary Service', 'Deliver Toiletries'].map(
+                              (value) => (
+                                <SelectItem key={value} value={value}>
+                                  {value}
+                                </SelectItem>
+                              )
+                            )}
+                          </SelectContent>
+                        </Select>
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+
+                {/* Product Category */}
+                <FormField
+                  control={form.control}
+                  name="productCategory"
+                  render={({ field }) => (
+                    <FormItem className="flex items-center gap-4">
+                      <FormLabel className="text-sm w-40 text-nowrap font-medium text-gray-700">
+                        Product category
+                      </FormLabel>
+                      <FormControl>
+                        <Input
+                          placeholder="Enter Description"
+                          {...field}
+                          className="bg-[#F6EEE0] w-64 text-gray-700 p-2 rounded-md border-none outline-none focus:ring-0 text-sm"
+                        />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+
+                {/* Product Name */}
+                <FormField
+                  control={form.control}
+                  name="productName"
+                  render={({ field }) => (
+                    <FormItem className="flex items-center gap-4">
+                      <FormLabel className="text-sm w-40 text-nowrap font-medium text-gray-700">
+                        Product name
+                      </FormLabel>
+                      <FormControl>
+                        <Input
+                          type="text"
+                          placeholder="Enter Product Name"
+                          {...field}
+                          className="bg-[#F6EEE0] w-64 text-gray-700 p-2 rounded-md border-none outline-none focus:ring-0 text-sm"
+                        />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+              </div>
+
+              {/* Product Image Upload */}
+              <div className="w-[30%]">
+                <FormField
+                  control={form.control}
+                  name="productImage"
+                  render={({ field }) => (
+                    <FormItem className="flex flex-col gap-2">
+                      <FormLabel className="text-sm w-40 font-medium text-gray-700">
+                        Product image
+                      </FormLabel>
+                      <FormControl>
+                        <div
+                          className="relative h-44 w-44 rounded-lg bg-[#F6EEE0]"
+                          onDrop={(e) => {
+                            e.preventDefault();
+                            const file = e.dataTransfer.files?.[0];
+                            if (file) {
+                              field.onChange(file);
+                              if (preview) URL.revokeObjectURL(preview);
+                              setPreview(URL.createObjectURL(file));
+                            }
+                          }}
+                          onDragOver={(e) => e.preventDefault()}
+                        >
+                          <div className="h-full w-full flex items-center justify-center">
+                            {preview && (
+                              <img
+                                src={preview}
+                                alt="Product preview"
+                                className="h-full w-full object-cover rounded-lg"
+                              />
+                            )}
+                          </div>
+                          {!preview && (
+                            <label
+                              htmlFor="fileUpload"
+                              className="absolute inset-0 flex justify-center items-center cursor-pointer"
+                              aria-label="Upload product image"
+                            >
+                              <PiCameraThin className="text-black w-12 h-44 opacity-30" />
+                            </label>
+                          )}
+                          <input
+                            type="file"
+                            accept="image/*"
+                            onChange={(e) => {
+                              const file = e.target.files?.[0];
+                              if (file) {
+                                if (!file.type.startsWith('image/')) {
+                                  alert('Please upload an image file.');
+                                  return;
+                                }
+                                if (file.size > 5 * 1024 * 1024) {
+                                  alert('File size exceeds 5MB.');
+                                  return;
+                                }
+                                if (preview) URL.revokeObjectURL(preview);
+                                const imageUrl = URL.createObjectURL(file);
+                                setPreview(imageUrl);
+                              } else {
+                                setPreview(null);
+                              }
+                              field.onChange(file);
+                            }}
+                            className="hidden"
+                            id="fileUpload"
+                          />
+                        </div>
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+              </div>
+            </div>
+            <div className="w-full h-[1px] bg-black opacity-15" />
+            {/* Submit Button */}
+            <div className="flex items-center gap-4 px-10">
+              <Button type="submit" className="btn-primary">
+                Save
+              </Button>
+              <Button onClick={() => router.push('/')} className="btn-primary">
+                View Products
+              </Button>
+            </div>
+          </form>
+        </Form>
+      </div>
+    </div>
+  );
+};
+
+export default AddItemModal;
